26acae73507a9312a121f97d1713417014ca7f45
unknown <I024139@SOFN60235824A.sap.corp>
2013-10-27 17:12:03 +0200

    Simple logging utility in JS.

--- js.tutil/scripts.js
+++ js.tutil/scripts.js
@@ -1,17 +1,40 @@
+function zeroPad(number, digits) {
+    var s = "00000" + number;
+    return s.substr(s.length - digits);
+}
+
+function timeString() {
+	d = new Date();
+	return d.getUTCHours() + 
+		":" + zeroPad(d.getUTCMinutes(), 2) + 
+		":" + zeroPad(d.getUTCSeconds(), 2) + 
+		"." + zeroPad(d.getUTCMilliseconds(), 3);
+}
+
 function log(message) {
 	logList = document.getElementById("LOG-ELEMENT");
 	if (logList != null) {
-		line = document.createElement("pre");
-		line.innerHTML = message;
-		logList.appendChild(line);
+		row = document.createElement("tr");
+		tData = document.createElement("td");
+		tText = document.createTextNode(timeString());
+		tData.appendChild(tText);
+		mData = document.createElement("td");
+		mData.setAttribute("style", "width: 100%;");
+		mText = document.createTextNode("" + message);
+		mData.appendChild(mText);
+		row.appendChild(tData);
+		row.appendChild(mData);
+		logList.appendChild(row);
 	} else {
 		body = document.getElementsByTagName("body")[0];
 		div = document.createElement("div");
 		div.id = "LOG-CONTAINER";
+		logPar = document.createElement("table");
+		logPar.id = "LOG-ELEMENT";
+		div.appendChild(logPar);
 		body.appendChild(div);
-		logList = document.createElement("ol");
-		logList.id = "LOG-ELEMENT";
-		div.appendChild(logList);
 		log(message);
 	}
 }
+
+
